6f75f73596c23985435997b702736883c41c59f0
Sorokin Anton <sorokin.a.developer@gmail.com>
2026-09-06 01:55:48 +0500

    Remove ServiceLocator support

--- screen-navigator-servicelocator/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module dev.sorokin.screennavigator.servicelocator {
-    requires transitive dev.sorokin.screennavigator.core;
-    requires transitive dev.sorokin.servicelocator.core;
-
-    exports dev.sorokin.screennavigator.servicelocator;
-}
